b2703f135f167567fbd7868c5c665cd8869bf8f0
Jean-Jérôme Pantalacci <jjpxiii@gmail.com>
2018-12-06 12:31:33 +0100

    added testAuth

---
+++ testAuth/index.js
@@ -0,0 +1,14 @@
+var http = require("http");
+exports.get = function(event, context) {
+  console.log("start request to " + event.url);
+  http
+    .get(event.url, function(res) {
+      console.log("Got response: " + res.statusCode);
+      context.succeed();
+    })
+    .on("error", function(e) {
+      console.log("Got error: " + e.message);
+      context.done(null, "FAILURE");
+    });
+  console.log("end request to " + event.url);
+};
